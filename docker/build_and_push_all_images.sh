222113368c8b543f86c83c59ab20a8e6af1dd1ed
redaellimattia <redaelli.mattia10@gmail.com>
2023-03-06 16:53:02 +0100

    refactor: ♻️ move .env inside docker/runner

--- docker/build_and_push_all_images.sh
+++ docker/build_and_push_all_images.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-cd ..
-source .env
+source runner/.env
 echo "Reading docker-hub username from .env: ${DOCKERHUB_USERNAME}"
-cd docker || exit
 
 cd bft_benchmark && ./build_and_push.sh ${DOCKERHUB_USERNAME} && cd ..
 cd kabis_benchmark && ./build_and_push.sh ${DOCKERHUB_USERNAME} && cd ..
