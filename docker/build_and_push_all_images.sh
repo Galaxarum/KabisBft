336a7a3940a3dde10404cf8b3ba1f5fbcb30c19b
redaellimattia <redaelli.mattia10@gmail.com>
2023-03-06 16:20:17 +0100

    feat: ✨ add .env file

--- docker/build_and_push_all_images.sh
+++ docker/build_and_push_all_images.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+cd ..
+source .env
+echo "Reading docker-hub username from .env: ${DOCKERHUB_USERNAME}"
+cd docker || exit
 
-read -p "Enter docker hub username: " USER
-
-cd bft_benchmark && ./build_and_push.sh $USER && cd ..
-cd kabis_benchmark && ./build_and_push.sh $USER && cd ..
-cd kafka_benchmark && ./build_and_push.sh $USER && cd ..
+cd bft_benchmark && ./build_and_push.sh ${DOCKERHUB_USERNAME} && cd ..
+cd kabis_benchmark && ./build_and_push.sh ${DOCKERHUB_USERNAME} && cd ..
+cd kafka_benchmark && ./build_and_push.sh ${DOCKERHUB_USERNAME} && cd ..
