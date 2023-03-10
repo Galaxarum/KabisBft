f445457f0b131924fb63278bf0d002b8ee015f41
Flavio Rizzoglio <flavio.rizzoglio@mail.polimi.it>
2023-03-10 10:21:41 +0100

    chore: 🙈 updated .gitignore

--- docker/runner/run.sh
+++ docker/runner/run.sh
@@ -2,16 +2,15 @@ KABIS_ENVS=envs/kabis
 KAFKA_ENVS=envs/kafka
 
 docker system prune -a -f
+
 for conf in "$KABIS_ENVS"/*; do
   docker compose --file "kabis-compose.yaml" --env-file "$conf" up --force-recreate --abort-on-container-exit
-  docker system prune -a -f
   echo "kabis: $conf"
 done
 
 for type in kafka bft; do
 for conf in "$KAFKA_ENVS"/*; do
   docker compose --file "$type-compose.yaml" --env-file "$conf" up --force-recreate --abort-on-container-exit
-  docker system prune -a -f
   echo "$type: $conf"
 done
 done
