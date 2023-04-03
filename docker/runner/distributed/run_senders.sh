7cbfe5b97297678a49c14fe55886d8a237a65f42
Flavio Rizzoglio <flaviorizzoglio@gmail.com>
2023-04-03 14:18:43 +0200

    fix: ✏️ fixed typos in run scripts for distributed benchmarks

--- docker/runner/distributed/run_senders.sh
+++ docker/runner/distributed/run_senders.sh
@@ -4,13 +4,13 @@ KAFKA_ENVS=../envs/kafka
 docker system prune -a -f
 
 for conf in "$KABIS_ENVS"/*; do
-  docker compose --file "kabis-compose-receivers.yaml" --env-file "$conf" up --force-recreate --abort-on-container-exit
+  docker compose --file "kabis-compose-senders.yaml" --env-file "$conf" up --force-recreate --abort-on-container-exit
   echo "kabis: $conf"
 done
 
 for type in kafka bft; do
 for conf in "$KAFKA_ENVS"/*; do
-  docker compose --file "$type-compose-receivers.yaml" --env-file "$conf" up --force-recreate --abort-on-container-exit
+  docker compose --file "$type-compose-senders.yaml" --env-file "$conf" up --force-recreate --abort-on-container-exit
   echo "$type: $conf"
 done
 done
