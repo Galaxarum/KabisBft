a6c0f4dd993930955ac331a3845f1ab4c477b6e4
redaellimattia <redaelli.mattia10@gmail.com>
2023-06-27 12:56:39 +0200

    refactor: ♻️update headers print

--- src/main/java/kabis/art_exhibition/ArtExhibitionConsumer.java
+++ src/main/java/kabis/art_exhibition/ArtExhibitionConsumer.java
@@ -51,7 +51,7 @@ public abstract class ArtExhibitionConsumer {
             ConsumerRecords<Integer, String> records = consumer.poll(POLL_TIMEOUT);
             for (ConsumerRecord<Integer, String> record : records) {
                 i += 1;
-                System.out.println("Received " + record.value() + " from " + record.key());
+                System.out.println("Received " + record.value() + " from " + record.key() + " with header: " + record.headers().lastHeader("sender"));
             }
             //i += consumer.poll(POLL_TIMEOUT).count();
         }
