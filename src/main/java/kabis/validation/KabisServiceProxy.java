bf4f2bbe85c2a049458a6a57732289f9625871d1
Flavio Rizzoglio <flavio.rizzoglio@mail.polimi.it>
2023-07-02 14:26:43 +0200

    wip: 🚧 added print for DEBUG

--- src/main/java/kabis/validation/KabisServiceProxy.java
+++ src/main/java/kabis/validation/KabisServiceProxy.java
@@ -11,47 +11,49 @@ import java.util.List;
 public class KabisServiceProxy {
     private final ServiceProxy bftServiceProxy;
     private final boolean orderedPulls;
-    private int nextPullIndex =0;
-    
-    public void push(SecureIdentifier sid){
-        try(var bytes = new ByteArrayOutputStream()){
+    private int nextPullIndex = 0;
+
+    public void push(SecureIdentifier sid) {
+        try (var bytes = new ByteArrayOutputStream()) {
             bytes.write(OPS.PUSH.ordinal());
             bytes.writeBytes(sid.serialize());
             bftServiceProxy.invokeOrdered(bytes.toByteArray());
-        }catch (IOException e){
+        } catch (IOException e) {
             throw new SerializationException(e);
         }
     }
-    
-    public List<SecureIdentifier> pull(){
-        try(var bytes = new ByteArrayOutputStream()){
+
+    public List<SecureIdentifier> pull() {
+        try (var bytes = new ByteArrayOutputStream()) {
             bytes.write(OPS.PULL.ordinal());
             bytes.writeBytes(ByteBuffer.allocate(Integer.BYTES).putInt(nextPullIndex).array());
             var request = bytes.toByteArray();
             var responseBytes = orderedPulls ?
                     bftServiceProxy.invokeOrdered(request) :
                     bftServiceProxy.invokeUnordered(request);
-            if(responseBytes==null || responseBytes.length==0){
+            if (responseBytes == null || responseBytes.length == 0) {
+                System.out.println("[KabisServiceProxy] pull method returns responseBytes as null or with length 0");
                 try {
                     Thread.sleep(50);
-                }catch (InterruptedException ignored){}
+                } catch (InterruptedException ignored) {
+                }
                 return List.of();
             }
             var result = KabisServiceReplica.deserializeSidList(responseBytes);
             nextPullIndex += result.size();
             return result;
-        }catch (IOException e){
+        } catch (IOException e) {
             throw new SerializationException(e);
         }
     }
 
-    public KabisServiceProxy(int id,boolean orderedPulls){
+    public KabisServiceProxy(int id, boolean orderedPulls) {
         this.bftServiceProxy = new ServiceProxy(id);
         this.orderedPulls = orderedPulls;
     }
 
-    public KabisServiceProxy(int id){
-        this(id,false);
+    public KabisServiceProxy(int id) {
+        this(id, false);
     }
 
 }
