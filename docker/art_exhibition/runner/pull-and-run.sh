34d839b6429c11b1bfd28706e6fbaa34323f1c3c
redaellimattia <redaelli.mattia10@gmail.com>
2023-07-04 11:51:32 +0200

    refactor: ♻️ update pull-and-run.sh

--- docker/art_exhibition/runner/pull-and-run.sh
+++ docker/art_exhibition/runner/pull-and-run.sh
@@ -2,4 +2,4 @@ docker compose --file $1 --env-file $2 rm -f
 
 docker compose --file $1 --env-file $2 pull
 
-docker compose --file $1 --env-file $2 up --remove-orphans --force-recreate
+docker compose --file $1 --env-file $2 up --remove-orphans --force-recreate --abort-on-container-exit
