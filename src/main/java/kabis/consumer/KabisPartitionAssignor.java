7126bd6d23c05e2634fcff7e6b5a733f537eb590
Redaelli Mattia <redaelli.mattia10@gmail.com>
2023-07-20 16:35:38 +0200

    refactor: ♻️ update assignor logic

--- src/main/java/kabis/consumer/KabisPartitionAssignor.java
+++ src/main/java/kabis/consumer/KabisPartitionAssignor.java
@@ -3,42 +3,25 @@ package kabis.consumer;
 import org.apache.kafka.clients.consumer.internals.AbstractPartitionAssignor;
 import org.apache.kafka.common.Configurable;
 import org.apache.kafka.common.TopicPartition;
-import org.apache.kafka.common.config.AbstractConfig;
-import org.apache.kafka.common.config.ConfigDef;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class KabisPartitionAssignor extends AbstractPartitionAssignor implements Configurable {
-    private final Logger log = LoggerFactory.getLogger(KabisPartitionAssignor.class);
     public static final String KABIS_ASSIGNOR_NAME = "kabis-assignor";
-
-    private KabisPartitionAssignorConfig config = new KabisPartitionAssignorConfig(new HashMap<>());
+    private final Logger log = LoggerFactory.getLogger(KabisPartitionAssignor.class);
+    private KabisPartitionAssignorConfig config;
 
     @Override
     public String name() {
         return KABIS_ASSIGNOR_NAME;
     }
 
-    private Map<String, List<MemberInfo>> consumersPerTopic(Map<String, Subscription> consumerMetadata) {
-        Map<String, List<MemberInfo>> topicToConsumers = new HashMap<>();
-        for (Map.Entry<String, Subscription> subscriptionEntry : consumerMetadata.entrySet()) {
-            String consumerId = subscriptionEntry.getKey();
-            MemberInfo memberInfo = new MemberInfo(consumerId, subscriptionEntry.getValue().groupInstanceId());
-            for (String topic : subscriptionEntry.getValue().topics()) {
-                put(topicToConsumers, topic, memberInfo);
-            }
-        }
-        return topicToConsumers;
-    }
-
     @Override
     public Map<String, List<TopicPartition>> assign(Map<String, Integer> partitionsPerTopic,
                                                     Map<String, Subscription> subscriptions) {
         Map<String, List<MemberInfo>> consumersPerTopic = consumersPerTopic(subscriptions);
-        int numberOfConsumersPerGroup = 5;
 
         Map<String, List<TopicPartition>> assignment = new HashMap<>();
         for (String memberId : subscriptions.keySet())
@@ -51,6 +34,18 @@ public class KabisPartitionAssignor extends AbstractPartitionAssignor implements
             Integer numPartitionsForTopic = partitionsPerTopic.get(topic);
             if (numPartitionsForTopic == null)
                 continue;
+
+            String[] consumerIds = config.groupConsumersIds();
+
+            int numberOfConsumersPerGroup = consumerIds.length;
+
+            List<MemberInfo> consumersList = new ArrayList<>();
+            for (int i = 0; i < numberOfConsumersPerGroup; i++) {
+                String consumerId = consumersForTopic.get(i).groupInstanceId.orElse(null);
+                if (consumerId == null)
+                    continue;
+                consumerId = consumerId.substring(consumerId.lastIndexOf("-") + 1);
+            }
 
             Collections.sort(consumersForTopic);
 
@@ -67,31 +62,20 @@ public class KabisPartitionAssignor extends AbstractPartitionAssignor implements
         return assignment;
     }
 
+    private Map<String, List<MemberInfo>> consumersPerTopic(Map<String, Subscription> consumerMetadata) {
+        Map<String, List<MemberInfo>> topicToConsumers = new HashMap<>();
+        for (Map.Entry<String, Subscription> subscriptionEntry : consumerMetadata.entrySet()) {
+            String consumerId = subscriptionEntry.getKey();
+            MemberInfo memberInfo = new MemberInfo(consumerId, subscriptionEntry.getValue().groupInstanceId());
+            for (String topic : subscriptionEntry.getValue().topics()) {
+                put(topicToConsumers, topic, memberInfo);
+            }
+        }
+        return topicToConsumers;
+    }
+
     @Override
     public void configure(Map<String, ?> configs) {
         this.config = new KabisPartitionAssignorConfig(configs);
-        this.log.info("Configured KabisPartitionAssignor with {}", this.config.groupConsumersIds());
-    }
-}
-
-class KabisPartitionAssignorConfig extends AbstractConfig {
-    public static final String GROUP_CONSUMERS_IDS_CONFIG = "group.consumers.ids";
-    public static final String GROUP_CONSUMERS_IDS_DOC = "List of consumers ids in the group";
-
-    private static final ConfigDef CONFIG;
-
-    static {
-        CONFIG = new ConfigDef()
-                .define(GROUP_CONSUMERS_IDS_CONFIG, ConfigDef.Type.INT, Integer.MAX_VALUE,
-                        ConfigDef.Importance.HIGH, GROUP_CONSUMERS_IDS_DOC);
-    }
-
-    public KabisPartitionAssignorConfig(final Map<?, ?> originals) {
-        super(CONFIG, originals);
-    }
-
-    public List<Integer> groupConsumersIds() {
-        String groupConsumersIds = getString(GROUP_CONSUMERS_IDS_CONFIG);
-        return Arrays.stream(groupConsumersIds.split(",")).map(Integer::parseInt).collect(Collectors.toList());
     }
 }
