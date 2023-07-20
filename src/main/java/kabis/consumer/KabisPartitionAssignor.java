e5cd224c9e9499485dcb6bfcd5912ca94101bde4
Flavio Rizzoglio <flavio.rizzoglio@mail.polimi.it>
2023-07-20 14:08:28 +0200

    refactor: ♻️ added custom partition assignor property

--- src/main/java/kabis/consumer/KabisPartitionAssignor.java
+++ src/main/java/kabis/consumer/KabisPartitionAssignor.java
@@ -1,15 +1,21 @@
 package kabis.consumer;
 
 import org.apache.kafka.clients.consumer.internals.AbstractPartitionAssignor;
+import org.apache.kafka.common.Configurable;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.config.AbstractConfig;
+import org.apache.kafka.common.config.ConfigDef;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
-public class KabisPartitionAssignor extends AbstractPartitionAssignor {
-    private static final Logger log = LoggerFactory.getLogger(KabisPartitionAssignor.class);
+public class KabisPartitionAssignor extends AbstractPartitionAssignor implements Configurable {
+    private final Logger log = LoggerFactory.getLogger(KabisPartitionAssignor.class);
     public static final String KABIS_ASSIGNOR_NAME = "kabis-assignor";
+
+    private KabisPartitionAssignorConfig config = new KabisPartitionAssignorConfig(new HashMap<>());
 
     @Override
     public String name() {
@@ -59,5 +65,33 @@ public class KabisPartitionAssignor extends AbstractPartitionAssignor {
             }
         }
         return assignment;
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+        this.config = new KabisPartitionAssignorConfig(configs);
+        this.log.info("Configured KabisPartitionAssignor with {}", this.config.groupConsumersIds());
+    }
+}
+
+class KabisPartitionAssignorConfig extends AbstractConfig {
+    public static final String GROUP_CONSUMERS_IDS_CONFIG = "group.consumers.ids";
+    public static final String GROUP_CONSUMERS_IDS_DOC = "List of consumers ids in the group";
+
+    private static final ConfigDef CONFIG;
+
+    static {
+        CONFIG = new ConfigDef()
+                .define(GROUP_CONSUMERS_IDS_CONFIG, ConfigDef.Type.INT, Integer.MAX_VALUE,
+                        ConfigDef.Importance.HIGH, GROUP_CONSUMERS_IDS_DOC);
+    }
+
+    public KabisPartitionAssignorConfig(final Map<?, ?> originals) {
+        super(CONFIG, originals);
+    }
+
+    public List<Integer> groupConsumersIds() {
+        String groupConsumersIds = getString(GROUP_CONSUMERS_IDS_CONFIG);
+        return Arrays.stream(groupConsumersIds.split(",")).map(Integer::parseInt).collect(Collectors.toList());
     }
 }
