845e0ac3d6b8218e20a0f1af8dde0a24b52efed9
Flavio Rizzoglio <flavio.rizzoglio@mail.polimi.it>
2023-08-10 11:04:16 +0200

    refactor: ♻️ updated kafka-only art exhibition case study

--- src/main/java/kabis/art_exhibition/kafka_only/SafeCorp.java
+++ src/main/java/kabis/art_exhibition/kafka_only/SafeCorp.java
@@ -5,10 +5,15 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.TopicPartition;
 
 import java.time.Duration;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 import static java.lang.Integer.parseInt;
 
@@ -45,13 +50,16 @@ public class SafeCorp extends ArtExhibitionProducer {
         KafkaProducer<Integer, String> safeCorpProducer = new KafkaProducer<>(producerProperties);
         System.out.println("[SafeCorp - Kafka Only] Kabis Producer created");
 
+        List<TopicPartition> assignedPartitions = new ArrayList<>(safeCorpConsumer.assignment());
+        List<Integer> assignedPartitionsIDs = assignedPartitions.stream().map(TopicPartition::partition).collect(Collectors.toList());
+
         // -- READ TRUE AND FALSE ALARMS AND RESPOND --
         System.out.println("[SafeCorp - Kafka Only] Reading alarms");
         String responseMessage = "[SafeCorp - Kafka Only] ALARM RECEIVED ";
-        int numberOfAssignedPartitions = safeCorpConsumer.assignment().size();
+        int numberOfAssignedPartitions = assignedPartitions.size();
         System.out.println("[SafeCorp - Kafka Only] Number of assigned exhibitions: " + numberOfAssignedPartitions);
         int recordsToRead = (getNumberOfTrueAlarms() + getNumberOfFalseAlarms()) * numberOfAssignedPartitions;
-        long receivingTime = pollAndRespondMeasure(safeCorpConsumer, safeCorpProducer, recordsToRead, responseMessage);
+        LocalTime[] pollTimeResults = pollAndRespondMeasure(safeCorpConsumer, safeCorpProducer, recordsToRead, responseMessage);
         safeCorpConsumer.close();
 
         System.out.println("[SafeCorp - Kafka Only] READING DONE! Consumer Closed");
@@ -59,21 +67,21 @@ public class SafeCorp extends ArtExhibitionProducer {
         // -- SEND UNCAUGHT ALARMS --
         System.out.println("[SafeCorp - Kafka Only] Sending uncaught breaches");
         String sendMessage = "[SafeCorp - Kafka Only] BREACH FOUND ";
-        long sendingTime = sendAndMeasure(safeCorpProducer, getNumberOfUncaughtBreaches(), sendMessage);
+        LocalTime[] pushTimeResults = sendAndMeasureCertainPartitions(safeCorpProducer, assignedPartitionsIDs, getNumberOfUncaughtBreaches(), sendMessage);
 
-        long totalTime = sendingTime + receivingTime;
+        LocalTime[] timeResults = new LocalTime[]{pollTimeResults[0], pushTimeResults[1]};
         safeCorpProducer.close();
         System.out.println("[SafeCorp - Kafka Only] DONE! Producer Closed - Saving experiments");
 
         // Store results
-        ArtExhibitionBenchmarkResult.storeThroughputToDisk(Arrays.asList("#EXHIBITIONS", "#TRUE-ALARMS", "#UNCAUGHT-BREACHES", "TOTAL TIME [ns]"),
-                Arrays.asList(Integer.toString(getNumberOfArtExhibitions()), Integer.toString(getNumberOfTrueAlarms()), Integer.toString(getNumberOfUncaughtBreaches()), Long.toString(totalTime)));
+        ArtExhibitionBenchmarkResult.storeThroughputToDisk(Arrays.asList("#EXHIBITIONS", "#TRUE-ALARMS", "#UNCAUGHT-BREACHES", "START TIME", "END TIME"),
+                Arrays.asList(Integer.toString(getNumberOfArtExhibitions()), Integer.toString(getNumberOfTrueAlarms()), Integer.toString(getNumberOfUncaughtBreaches()), timeResults[0].toString(), timeResults[1].toString()));
         System.out.println("[SafeCorp - Kafka Only] Experiments persisted!");
     }
 
-    protected long pollAndRespondMeasure(KafkaConsumer<Integer, String> consumer, KafkaProducer<Integer, String> producer, Integer recordsToRead, String message) {
+    protected LocalTime[] pollAndRespondMeasure(KafkaConsumer<Integer, String> consumer, KafkaProducer<Integer, String> producer, Integer recordsToRead, String message) {
         int i = 0;
-        long t1 = System.nanoTime();
+        LocalTime startTime = LocalTime.now();
         System.out.println("[pollAndRespondMeasure - Kafka Only]: recordsToRead: " + recordsToRead + " with POLL_TIMEOUT: " + POLL_TIMEOUT);
         while (i < recordsToRead) {
             ConsumerRecords<Integer, String> records = consumer.poll(POLL_TIMEOUT);
@@ -82,14 +90,31 @@ public class SafeCorp extends ArtExhibitionProducer {
                 if (!recordMessage.contains("[SafeCorp - Kafka Only]")) {
                     i += 1;
                     ProducerRecord<Integer, String> responseRecord = new ProducerRecord<>(Topics.ART_EXHIBITION.toString(), record.partition(), record.key(), message + recordMessage);
-                    System.out.println("[pollAndRespondMeasure - Kafka Only]: Sending " + responseRecord.value() + " exhibition: " + responseRecord.key());
+                    System.out.println("[pollAndRespondMeasure - Kafka Only]: Sending " + responseRecord.value() + " exhibition: " + responseRecord.partition());
                     producer.send(responseRecord);
                 }
             }
         }
-        long t2 = System.nanoTime();
+        LocalTime endTime = LocalTime.now();
         System.out.println("[pollAndRespondMeasure - Kafka Only]: All messages read!");
 
-        return t2 - t1;
+        return new LocalTime[]{startTime, endTime};
+    }
+
+    protected LocalTime[] sendAndMeasureCertainPartitions(KafkaProducer<Integer, String> producer, List<Integer> partitions, Integer numberOfAlarms, String message) {
+        LocalTime startTime = LocalTime.now();
+        System.out.println("[sendAndMeasureCertainPartitions - Kafka Only]: numberOfArtExhibitions: " + partitions.size() + " numberOfAlarms:" + numberOfAlarms);
+        partitions.forEach(partition -> {
+            for (int i = 0; i < numberOfAlarms; i++) {
+                ProducerRecord<Integer, String> record = new ProducerRecord<>(kabis.art_exhibition.Topics.ART_EXHIBITION.toString(), partition, partition, message + i);
+                System.out.println("[sendAndMeasureCertainPartitions - Kafka Only]: Sending " + record.value() + " exhibition: " + record.partition());
+                producer.send(record);
+            }
+        });
+        LocalTime endTime = LocalTime.now();
+        System.out.println("[sendAndMeasureCertainPartitions - Kafka Only]: All messages sent!");
+        producer.flush();
+
+        return new LocalTime[]{startTime, endTime};
     }
 }
